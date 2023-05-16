3cdc943cbc1377d3b35817f41d807c81f0a1e0b0
PapoyDev <albrooks.miracle@gmail.com>
2023-05-16 02:31:51 -0500

    update

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -92,7 +92,7 @@ const SignIn = () => {
 
     setIsLoading(true);
     const res = await auth("sign-in", values);
-    if (res.code !== 200) {
+    if (res.code === 200) {
       localStorage.setItem("accessToken", "res.accessToken");
       switch (role) {
         case "admin":
